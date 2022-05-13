cf43188af33a678d403f496f990b0fc6a91504e6
Marius Sæteraas <71336754+Mturalyon@users.noreply.github.com>
2022-05-13 16:39:04 +0200

    finnished enquiry page

--- src/components/pages/enquiry/Enquiry.js
+++ src/components/pages/enquiry/Enquiry.js
@@ -4,18 +4,26 @@ import { useParams, useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import FormError from "../../pages/login/FormError";
-import useAxios from "../../../hooks/useAxios";
+import { BASE_URL } from "../../../constants/api";
 
 import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
+import { ENQ_API } from "../../../constants/api";
+import CreateToken from "../contact/CreateToken";
+import axios from "axios";
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
 const schema = yup.object().shape({
     full_name: yup.string().required("Please enter a name").min(4, "Minimum 4 characters"),
     email: yup.string().email("Please enter a valid email").required("Please enter an email address"),
-    phone_number: yup.number().typeError("You must specify a number").required("Please provide with a phone number").min(6, "Too short number"),
-    number_of_guests: yup.number(),
+    phone_number: yup.string().matches(phoneRegExp, "Phone number is not valid"),
+    arrival_date: yup.date().required("Choose an arrival date").typeError("Choose your arrival date"),
+    departure_date: yup.date().required("Choose your departure date").typeError("Choose your departure date"),
+    number_of_guests: yup.number().typeError("Add number of guests").min(1, "Minimum 1 guest").max(8, "No more than 8 guests"),
 });
 
 function Enquiry() {
@@ -30,8 +38,6 @@ function Enquiry() {
         resolver: yupResolver(schema),
     });
 
-    const http = useAxios();
-
     let history = useNavigate();
 
     let { id } = useParams();
@@ -40,13 +46,13 @@ function Enquiry() {
         history("/hotel-all");
     }
 
-    const url = `wc/v3/products/${id}`;
+    const url = BASE_URL + `wc/store/products/${id}`;
 
     useEffect(
         function () {
             async function getHotel() {
                 try {
-                    const response = await http.get(url);
+                    const response = await axios.get(url);
                     setHotel(response.data);
                 }
                 catch (error) {
@@ -61,6 +67,13 @@ function Enquiry() {
         []
     );
 
+
+    const token = CreateToken();
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+    };
+
     async function onSubmit(data) {
         setUpdatingHotel(true);
         setUpdateError(null);
@@ -69,17 +82,20 @@ function Enquiry() {
         const enquiry = {
             title: data.title,
             content: "",
+            status: "publish",
             fields: {
                 full_name: data.full_name,
                 email: data.email,
                 phone_number: data.phone_number,
+                arrival_date: data.arrival_date,
+                departure_date: data.departure_date,
                 number_of_guests: data.number_of_guests,
                 message: data.message
             }
-        }
+        };
 
         try {
-
+            await axios.post(ENQ_API, enquiry, { headers });
             console.log(enquiry);
             setUpdated(true);
         }
@@ -111,9 +127,12 @@ function Enquiry() {
             <main className="form-main">
                 <div className="form-background"></div>
                 <form>
-                    <div className="update-complete">
+                    <div className="update-complete enquiry_update-complete">
                         <FontAwesomeIcon icon={faCircleCheck} className="update-icon" />
-                        <h2 className="update-heading">Reservation of: {hotel.name} complete!</h2>
+                        <h3 className="update-heading">Reservation of:</h3>
+                        <p className="enquiry_hotel-name">{hotel.name}</p>
+                        <h2>complete!</h2>
+                        <p className="enquiry_assurance">We will be back with you shortly</p>
                         <Link to="/hotel-all" className="update-link">Back</Link>
                     </div>
                 </form>
@@ -147,13 +166,27 @@ function Enquiry() {
 
                         <label>
                             Phone Number
-                            <input {...register("phone_number")} name="phone_number" placeholder="Phone Number.." type="number" />
+                            <input {...register("phone_number")} name="phone_number" placeholder="Phone Number.." />
                             {errors.phone_number && <FormError>{errors.phone_number.message}</FormError>}
                         </label>
 
+                        <div className="date-container">
+                            <label>
+                                Arrival
+                                <input {...register("arrival_date")} name="arrival_date" type="date" />
+                                {errors.arrival_date && <FormError>{errors.arrival_date.message}</FormError>}
+                            </label>
+
+                            <label>
+                                Departure
+                                <input {...register("departure_date")} name="departure_date" type="date" />
+                                {errors.departure_date && <FormError>{errors.departure_date.message}</FormError>}
+                            </label>
+                        </div>
+
                         <label>
                             How Many Guests?
-                            <input {...register("number_of_guests")} name="number_of_guests" type="number" placeholder="0" min={1} max={8} />
+                            <input {...register("number_of_guests")} name="number_of_guests" type="number" placeholder="0" />
                             {errors.number_of_guests && <FormError>{errors.number_of_guests.message}</FormError>}
                         </label>
 
